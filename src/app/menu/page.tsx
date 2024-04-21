728b39edbddb34ed406c74a30ea42e1fbbc05da4
Haifa Naim <70284775+hnaimm@users.noreply.github.com>
2024-04-21 03:18:18 +0300

    Update page.tsx

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,6 +2,8 @@ import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 const getData = async ()=>{
   const res = await fetch("http://localhost:3000/api/categories",{
     cache:"no-store"
